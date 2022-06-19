12a74dc3983443154abf7016decdce9fcecd111a
Jorge <fulano@email.com>
2022-06-19 00:12:48 -0300

    armazenamento OK

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ import express from 'express';
 import cors from 'cors';
 
 const server = express();
-
 server.use([cors(), express.json()]);
+
+const user = [];
+const tweets = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}];
+
+
+server.get('/sign-up', (req, res) => {
+    console.log(req.body);
+    res.send(user);
+})
+
+server.post('/sign-up', (req, res) => {
+    console.log(req.body);
+    user.push(req.body);
+    res.send(user);
+})
+
 
 server.listen(5000);
