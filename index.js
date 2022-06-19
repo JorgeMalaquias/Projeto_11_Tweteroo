2b7a5909ade5cc90cbd103b1a0ffd3eac25fcf1f
Jorge <fulano@email.com>
2022-06-19 01:17:20 -0300

    requisitos cumpridos

--- index.js
+++ index.js
@@ -4,22 +4,117 @@ import cors from 'cors';
 const server = express();
 server.use([cors(), express.json()]);
 
-const user = [];
+const user = [{
+    username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+}];
 const tweets = [{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}, {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}, {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}, {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+}, {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+},
+{
     username: "bobesponja",
     tweet: "eu amo o hub",
 }];
 
 
-server.get('/sign-up', (req, res) => {
-    console.log(req.body);
-    res.send(user);
-})
-
+function findingAvatar(userName) {
+    console.log("tamu na lida");
+    console.log(user);
+    const userToFindAvatar = user.find((u) => u.username === userName);
+    console.log("eis o que achei");
+    console.log(user.find((u) => u.username === userName));
+    return userToFindAvatar.avatar;
+}
+function tweet(t) {
+    const obj = {
+        username: t.username,
+        avatar: findingAvatar(t.username),
+        tweet: t.tweet
+    }
+    return obj;
+}
 server.post('/sign-up', (req, res) => {
-    console.log(req.body);
     user.push(req.body);
     res.send("OK");
+})
+server.post('/tweets', (req, res) => {
+    tweets.push(req.body);
+    res.send("OK");
+})
+
+server.get('/tweets', (req, res) => {
+    if (tweets.length > 10) {
+        console.log(tweets.length);
+        res.send((tweets.slice(tweets.length - 10, tweets.length)).map(tweet));
+    } else {
+        console.log(tweets.length);
+        res.send(tweets.map(tweet));
+    }
 })
 
 
